ed070fa2950c3b7d7889149974f784282d98aa68
Priyank <93072pri@gmail.com>
2020-08-01 12:14:48 +0530

    adding new files

---
+++ src/Sdata.jsx
@@ -0,0 +1,42 @@
+const Sdata = [
+  {
+    id: 1,
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/title/80100172",
+  },
+  {
+    id: 2,
+    imgsrc:
+      "https://stat1.bollywoodhungama.in/wp-content/uploads/2017/05/Malang.jpg",
+    title: "A Netflix Original Movie",
+    sname: "Malang",
+    link: "https://www.netflix.com/title/81214289",
+  },
+  {
+    id: 3,
+    imgsrc:
+      "https://m.media-amazon.com/images/M/MV5BNjQ0Mzk0OTE5MF5BMl5BanBnXkFtZTgwNDkyOTI0NTM@._V1_.jpg",
+    title: "A Netflix Original Series",
+    sname: "The kissing booth",
+    link: "https://www.netflix.com/in/title/80143556",
+  },
+  {
+    id: 4,
+    imgsrc:
+      "https://m.media-amazon.com/images/M/MV5BMWZkNzNlMzMtMjM5ZS00MWYzLWFmMmUtMjE1ODM3NjBlODA5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    title: "A Netflix Original Series",
+    sname: "the king",
+    link: "https://www.netflix.com/in/title/80182016",
+  },
+  {
+    id: 5,
+    imgsrc:
+      "https://m.media-amazon.com/images/M/MV5BZjMwNDQ4NzMtOThmZi00NmMyLThkMWItMTA3MTg2YjdiZDRmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    title: "A Netflix Original Series",
+    sname: "To All the Boys I've Loved Before",
+    link: "https://www.netflix.com/title/81030842",
+  },
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Cards from "./Cards";
+import Sdata from "./Sdata";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Ncard(val, ind, arr) {
+  return (
+    <Cards
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
+function Merge() {
+  return (
+    <>
+      <h1 className="heading_style">My top 5 netflix Series</h1>
+      {Sdata.map((val) => {
+        return (
+          <Cards
+            key={val.id}
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+          />
+        );
+      })}
+    </>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Merge />, document.getElementById("root"));
