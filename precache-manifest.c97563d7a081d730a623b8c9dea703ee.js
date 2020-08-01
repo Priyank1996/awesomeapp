93c16d2eb3900e334964ae6432ab3f80eb1b3b00
Priyank <93072pri@gmail.com>
2020-08-01 22:21:14 +0530

    Updates

---
+++ precache-manifest.c97563d7a081d730a623b8c9dea703ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1e21f1e240efbea8dd7b3a635dc620",
+    "url": "/awesomeapp/index.html"
+  },
+  {
+    "revision": "17a1d70783c203b7ca3a",
+    "url": "/awesomeapp/static/css/main.ed1d6c23.chunk.css"
+  },
+  {
+    "revision": "dfbe1a311b0830d01dca",
+    "url": "/awesomeapp/static/js/2.be51b0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesomeapp/static/js/2.be51b0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a1d70783c203b7ca3a",
+    "url": "/awesomeapp/static/js/main.c2937419.chunk.js"
+  },
+  {
+    "revision": "a0595463258c09e13d26",
+    "url": "/awesomeapp/static/js/runtime-main.7aff0e9b.js"
+  }
+]);
